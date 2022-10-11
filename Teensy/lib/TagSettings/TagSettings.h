ed2853baebaaf53539e43ab21ea1e3d810cb7511
Gabriel Antoniak <gjantoni@umich.edu>
2022-10-11 17:19:21 -0400

    Begins to add more comments to the TagSettings

--- Teensy/lib/TagSettings/TagSettings.h
+++ Teensy/lib/TagSettings/TagSettings.h
@@ -3,10 +3,21 @@
 #include <Arduino.h>
 
 #pragma pack(push, 1)
+/**
+ * @brief Settings for the ADS1299 EEG Chip
+ * 
+ * This is a union, to make more explicit that is contains both a structure containing the registers,
+ * as well as simply being an array of bytes that can be read from a \ref TagCommunicator sending a vector of bytes.
+ */
 union ADS1299Settings {
+    /**
+     * @brief The structure that stores the settable parameters of the ADS1299
+     * 
+     * For full details, see the ADS1299 data sheet
+     */
     struct ADSRegisters {
-        uint8_t config1 { 0x96 };
-        uint8_t config2 { 0xC0 };
+        uint8_t config1 { 0x96 }; /**< Config 1 - Res | Daisy Chain (0) or Multiple Read Back (1) | Oscillator Clock Disabled (0) or Enabled (1) | 1 | 0 | Sampling Rate (000 for 16 kSPS, 001 for 8 kSPS, ... 110 for 250 SPS) */
+        uint8_t config2 { 0xC0 }; /**< Config 2 - */
         uint8_t config3 { 0x60 };
         uint8_t loff { 0x00 };
         uint8_t chnset[8] { 0x61, 0x61, 0x61, 0x61, 0x00, 0x00, 0x00, 0x00 };
